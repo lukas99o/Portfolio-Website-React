52daf9d7d9e0421bc11361da2c3abc48da37b088
lukas99o <lukas99o@hotmail.com>
2024-05-28 18:09:34 +0200

    fixed contact form!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,9 @@
 /* global Email */
 import { useEffect } from "react";
 import profilepicture from "../images/profil-bild.jpg";
+import React from "react";
 
+/*
 function sendEmail(e) {
   e.preventDefault();
   Email.send({
@@ -21,6 +23,7 @@ function sendEmail(e) {
   );
   e.target.reset();
 }
+*/
 
 function Contact() {
   useEffect(() => {
@@ -73,16 +76,48 @@ function Contact() {
     };
   }, []);
 
+  // Send email part:
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ed313c56-6c98-4ee5-8ed3-b64937ce029b");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        alert("Message Sent Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        alert("Failed to send Message")
+        setResult(data.message);
+      } 
+    } catch (error) {
+      console.error("Error:", error);
+      setResult("An error occured while submitting the form.");
+    }
+  };
+
   return (
     <div className="contact-body">
       <div className="contact-container">
-        <form onSubmit={sendEmail}>
+        <form onSubmit={onSubmit}>
           <h3>Contact Me!</h3>
           <img src={profilepicture} alt="Profile" />
-          <input type="text" id="name" placeholder="Your Name" required />
-          <input type="email" id="email" placeholder="Email Id" required />
-          <input type="text" id="phone" placeholder="Phone no." required />
-          <textarea id="message" rows="4" placeholder="Type any message to me!" />
+          <input type="text" id="name" name="name" placeholder="Your Name" required />
+          <input type="email" id="email" name="email" placeholder="Email Id" required />
+          <textarea id="message" name="message" rows="4" placeholder="Type any message to me!" />
           <button type="submit">Send</button>
         </form>
       </div>
