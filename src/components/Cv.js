a682d4d9bbb01dbb207255ff28fba4fcd6792592
Lukas <lukas99o@hotmail.com>
2025-06-27 15:48:53 +0200

    made good stuff

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -20,13 +20,23 @@ function CV() {
                     <div className='cv-container'>
                         <header className="cv-header">
                             <h1>Lukas Olsson</h1>
-                            <div>
-                                <p>Home address: Småbrukets backe 60</p>
-                                <p>Mailing address: 141 58, Huddinge</p> 
+                            <h2>Contact</h2>
+                            <div className="cv-header-p">
+                                <div className="cv-header-left">
+                                    <p>Home address: Småbrukets backe 60</p>
+                                    <p>Mailing address: 141 58, Huddinge</p> 
+                                </div>
+                                <div className="cv-header-right">
+                                    <p>Phone number: 0762920163</p> 
+                                    <p>Email: lukas99o@hotmail.com</p>
+                                </div>
                             </div>
-                            <p>Phone number: 0762920163</p> 
-                            <p>Email: lukas99o@hotmail.com</p>
                         </header>
+
+                        <section>
+                            <h2>About Me</h2>
+                            <p>Newly graduated .NET Fullstack Developer with skills in C#, .NET, Entity Framework, Microsoft SQL, MVC, Azure, React, TypeScript, HTML, CSS, and agile methodologies. Interested in DevOps and CI/CD.</p>
+                        </section>
                         
                         <section className="cv-section">
                             <h2>Employment History</h2>
@@ -35,6 +45,7 @@ function CV() {
                                     <div><strong>Position:</strong> {job.details.position}, <strong>Employer:</strong> {job.details.employer}, <strong>Location:</strong> {job.details.location}</div>
                                     <div><strong>Dates:</strong> {job.dates.start_date} - {job.dates.end_date}</div>
                                     <div><strong>Description:</strong> {job.description}</div>
+                                    <br></br>
                                 </article>
                             ))}
                         </section>
@@ -46,6 +57,7 @@ function CV() {
                                     <div><strong>Program:</strong> {edu.program}, <strong>Institution:</strong> {edu.institution}, <strong>Location:</strong> {edu.location}</div>
                                     <div><strong>Dates:</strong> {edu.start_date} - {edu.end_date}</div>
                                     <div><strong>Description:</strong> {edu.description}</div>
+                                    <br></br>
                                 </article>
                             ))}
                         </section>
@@ -55,7 +67,6 @@ function CV() {
                             <div><strong>Driver's License:</strong> {CvData.additional_info.drivers_license}</div>
                             <div><strong>Languages:</strong> {CvData.additional_info.languages}</div>
                             <div><strong>References:</strong> {CvData.additional_info.references}</div>
-                            <div><strong>Current Study:</strong> {CvData.additional_info.current_study}</div>
                         </section>
                     </div>
                 </div>
--- src/components/Me.js
+++ src/components/Me.js
@@ -7,27 +7,29 @@ function Me() {
         <div className='me-body'>
             <div className="me-content">
                 <h1>Me</h1>
-                <p>
-                    My name is Lukas Olsson, and I live in Stockholm, Sweden, sharing an apartment with my lovely girlfriend and our cat. 
+                <div className="me-images">
+                    <img src={jag} alt="Picture of me"/>
+                    <img src={frida} alt="Picture of me and my girlfriend on a big rock by the sea."/>
+                </div>
+                
+                <div class="me-content-p">
+                    <p>
+                    <span>My</span> name is Lukas Olsson, and I live in Stockholm, Sweden, sharing an apartment with my lovely girlfriend and our cat. 
                     In my free time, I enjoy activities where movement is involved, such as going to the gym, walking in nature, going for runs and hanging out with my girlfriend and friends.
                     I also have a passion for coding my own projects and gaming whenever I find a quality game that captures my interest.
-                </p>
-                <br></br>
-                <p>
-                    My journey into the world of programming began about three years ago when I developed an interest in creating my own video game. 
-                    However, I soon realized that game development was far more challenging than I initially thoguht. So I started a self-learning journey to learn programming.
-                    I started my learning journey with FreeCodeCamp, a platform where I delved into HTML, CSS, and JavaScript.
-                </p>
-                <br></br>
-                <p>
-                    As my passion for programming grew, I decided to pursue formal education in the field. I decided to take a 2 year program at Chas Acadmey, where we learned .Net fullstack development. 
-                    I completed my studies may 20th 2025 and I am looking for any work related to development with a preference in .Net fullstack development.
-                </p>
-            </div>
-
-            <div className="me-images">
-                <img src={jag} alt="Picture of me"/>
-                <img src={frida} alt="Picture of me and my girlfriend on a big rock by the sea."/>
+                    </p>
+                    
+                    <p>
+                        My journey into the world of programming began about three years ago when I developed an interest in creating my own video game. 
+                        However, I soon realized that game development was far more challenging than I initially thoguht. So I started a self-learning journey to learn programming.
+                        I started my learning journey with FreeCodeCamp, a platform where I delved into HTML, CSS, and JavaScript.
+                    </p>
+                    
+                    <p>
+                        As my passion for programming grew, I decided to pursue formal education in the field. I decided to take a 2 year program at Chas Acadmey, where we learned .Net fullstack development. 
+                        I completed my studies may 20th 2025 and I am looking for any work related to development with a preference in .Net fullstack development.
+                    </p>
+                </div>
             </div>
         </div>
     )
